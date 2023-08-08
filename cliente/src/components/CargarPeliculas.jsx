470824540e40d6e9191922780d9cb4fb6141a80c
Angel Junior Curtido Quiñonez <106999344+angeljrcurtido@users.noreply.github.com>
2023-08-08 09:52:33 -0400

    Declaracion const CargarPeliculas.jsx

--- cliente/src/components/CargarPeliculas.jsx
+++ cliente/src/components/CargarPeliculas.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import './style.css'
-
+//Declaracion de const
 const CargarPeliculas = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
